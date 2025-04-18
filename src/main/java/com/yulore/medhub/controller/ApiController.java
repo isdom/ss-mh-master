75ab17e22f7a0b48b8dea546cde01fb5bb914f2e
ming.ma <ming.ma@yulore.com>
2025-04-18 10:32:37 +0800

    remove unused code

--- src/main/java/com/yulore/medhub/controller/ApiController.java
+++ src/main/java/com/yulore/medhub/controller/ApiController.java
@@ -1,7 +1,6 @@
 package com.yulore.medhub.controller;
 
 import com.yulore.medhub.LocalMasterService;
-import com.yulore.util.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.async.DeferredResult;
 
 @Controller
 @Slf4j
